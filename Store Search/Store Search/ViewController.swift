70be409f0cbc4c2283c6ae7a495fa5bc3e10ae4b
Jon <jonlz@github.com>
2015-10-01 21:50:46 -0400

    Added basic table view and search bar functionality

--- Store Search/Store Search/ViewController.swift
+++ Store Search/Store Search/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchResults = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0,
+            right: 0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake Result %d for '%@'", i,searchBar.text!))
+        }
+        tableView.reloadData()
+    }
+        
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        return .TopAttached
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(tableView: UITableView,cellForRowAtIndexPath indexPath: NSIndexPath) ->UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+    if cell == nil {
+        cell = UITableViewCell(style: .Default,reuseIdentifier: cellIdentifier)
+    }
+    cell.textLabel!.text = searchResults[indexPath.row]
+    return cell
+    }
+}
